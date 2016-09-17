f9cbe3fb595303771bb051ea0528d1768ecb9770
Muflih Kholidin <muflic.24@gmail.com>
2016-09-17 17:47:03 +0700

    http api route

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import Routes from './routes';
 
 Vue.use(VueRouter)
 
-Vue.http.options.root = 'http://localhost:8081/api';
+//Vue.http.options.root = 'http://localhost:8081/api';
+Vue.http.options.root = 'https://vue-simpleapp.herokuapp.com/api';
 
 var router = new VueRouter()
 
