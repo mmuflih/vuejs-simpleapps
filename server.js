58d33dd338ac68b094b94a321a3c20d5c88a1e03
Muflih Kholidin <muflic.24@gmail.com>
2016-09-18 07:08:44 +0700

    Fixing server with proxy

--- server.js
+++ server.js
@@ -2,14 +2,27 @@
 * @Author: Muhammad Muflikh Kholidin
 * @Date:   2016-09-17 12:58:49
 * @Last Modified by:   Muflih Kholidin
-* @Last Modified time: 2016-09-17 13:01:56
+* @Last Modified time: 2016-09-18 07:00:32
 */
 
 var express = require('express');
+var proxy = require('http-proxy-middleware');
 var path = require('path');
 var serveStatic = require('serve-static')
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 8081;
-app.listen(port);
+
+var apiProxy = proxy('/api', {
+        target: 'http://kholid.in:8080',
+        changeOrigin: true,
+        pathRewrite: {
+            '^/api': ''
+        }
+    }
+);
+
+var app = express();
+    app.use(serveStatic(path.join(__dirname, 'dist')));
+    app.use('/api', apiProxy);
+    app.listen(port);
 console.log('server started '+port);
